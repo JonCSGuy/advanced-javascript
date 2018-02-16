b09f209b84ff6be34cdd56d208864e924cc1b9ac
Jon <jon@jadx.ca>
2018-02-16 08:58:16 -0500

    Fix missing .babelrc, change move dev dependencies to dependencies.

---
+++ class/week4/calculator/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Calculator = (props) => {
+  return (
+    <div>
+      Calculator components will go here.
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Calculator/>,
+  document.getElementById('root')
+);
